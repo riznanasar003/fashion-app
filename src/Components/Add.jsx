66e41283e7f9821e7619de9176a64e48f4898ec8
riznanasar003 <richurizna10@gmail.com>
2024-06-13 03:32:12 +0530

    created

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavBar from './NavBar'
+import axios from 'axios'
 
 const Add = () => {
+    const [data,setdata] = useState(
+        {
+            "fid":"",
+            "fname":"",
+            "price":"",
+            "image":""
+        }
+    )
+    const inputHandler = (event) =>{
+        setdata({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue = () =>{
+        console.log(data)
+        axios.post("http://localhost:8080/add",data).then(
+            (response)=>{
+                console.log(data)
+                if (response.data.status=="success") {
+                    alert("Successfully Added")
+                    
+                } else {
+                    alert("Error")
+                    
+                }
+            }
+        ).catch()
+    }
   return (
     <div>
-
+            <NavBar/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -11,31 +40,31 @@ const Add = () => {
                             <div className="col col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor=".form-label">FASHION ID</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="fid" value={data.fid} onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor=".form-label">FASHION NAME</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='fname' value={data.fname} onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor=".form-label">PRICE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="price" value={data.price} onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor=".form-label">IMAGE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='image' value={data.image} onChange={inputHandler}/>
 
 
                             </div>
                             <div className="col col-sm-6 col-md-6 col-lg-6 col-xl-12 col-xxl-12">
 
-                            <button className="btn btn-success">ADD</button>
+                            <button className="btn btn-success" onClick={readValue}>ADD</button>
 
                             </div>
                         </div>
--- src/Components/ViewAll.jsx
+++ src/Components/ViewAll.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NavBar from './NavBar'
+import axios from 'axios'
 
 const ViewAll = () => {
+    const [data,changedData] = useState([])
+    const fetchData = () =>{
+        axios.post("http://localhost:8080/view",data).then(
+            (response)=>{
+                changedData(response.data)
+            }
+        ).catch().finally()
+    }
+    useEffect(()=>{fetchData()},[])
     return (
         <div>
-
+            <NavBar/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col xl-3 col-xxl-3">
+                            {data.map(
+                                (value, index)=>{
+                                    return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col xl-3 col-xxl-3">
 
-                                <div class="card">
-                                    <img src="..." class="card-img-top" alt="..."/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                                        </div>
+                                    <div class="card">
+                                        <img src={value.image} class="card-img-top" alt="..." height="300px"/>
+                                            <div class="card-body">
+                                                <h5 class="card-title">{value.fname}</h5>
+                                                <p class="card-text">{value.fid}</p>
+                                                <p class="card-text">{value.price}</p>
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                            </div>
+                                    </div>
+    
                                 </div>
-
-                            </div>
+                                }
+                            )}
                         </div>
                     </div>
                 </div>
